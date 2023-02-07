9cc9d61785ad416cc8c655b9948d0155ccdbff3e
webmasternoor <webmaster.noor@gmail.com>
2023-02-07 20:03:45 +0900

    line api social response data added to database

--- app/Http/Controllers/LineLoginController.php
+++ app/Http/Controllers/LineLoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TougouUser;
+use App\Models\TougouUserInfo;
 use App\Models\TougouUserSocialLogin;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
@@ -45,6 +47,7 @@ class LineLoginController extends Controller
                 $lineinfo = json_decode((string) $userRes->getBody(), true);
                 // dd($lineinfo);
                 $lineId = $lineinfo['providers']['line']['line_id'];
+                // $email = $lineinfo['user']['primary_key'];
             } catch (\Exception $e) {
                 return response()->json('', 400);
                 // return view('2021_user.login',['lineId'=>[]]);
@@ -60,14 +63,82 @@ class LineLoginController extends Controller
             ->first();
         // dd($integratedUser);
 
-        if ($integratedUser) {
-            dd('yes');
+        if ($integratedUser) {                  
+            
+            dd('You are already Registered');
+
             // DO LOGIN
             return redirect(Config::get('app.app1URL') . 'user/login')->with(['uniqueSocialPlusId' => $uniqueSocialPlusId]);
             // return response()->json($integratedUser);
         } else {
-            dd('no');
-            // NEW MEMBER to add TougouUserSocialLogin
+
+            $tougouDbUser = TougouUser::updateOrCreate([                    
+                'email' => $lineinfo['user']['primary_key'],
+                'password' => '123456',
+            ]);
+            
+            if ($tougouDbUser->wasRecentlyCreated) {
+                $tougouDbUser->user_infos()->saveMany([
+                    new TougouUserInfo([
+                        'lastname' => '',
+                        'firstname' => '',
+                        'lastname_kana' => '',
+                        'firstname_kana' => '',
+                        'birthdate' => '2023-02-03 16:52:51',
+                        'zip' => '',
+                        'address1' => '',
+                        'address2' => '',
+                        
+                        'college_type' => '',
+                        'college_name' => '',
+                        'graduation_year' => '',
+                        'medical_license_year' => '',
+                        'medical_license_month' => '',
+                        'medical_license_day' => '',
+                        'desired_working_j' => '',
+                        'desired_working_h' => '',
+                        'desired_working_s' => '',
+                        'desired_working_k' => '',
+                        
+                        'j_location1' => '',
+                        'j_location2' => '',
+                        'j_location3' => '',
+                        'h_location1' => '',
+                        'h_location2' => '',
+                        'h_location3' => '',
+                        's_location1' => '',
+                        's_location2' => '',
+                        's_location3' => '',
+                        'k_location1' => '',
+                        'k_location2' => '',
+                        'k_location3' => '',
+
+                        'j_subject' => '',
+                        'j_subject_others' => '',
+                        'h_subject' => '',
+                        'h_subject_others' => '',
+                        's_subject' => '',
+                        's_subject_others' => '',
+                        
+                        'doctor_id' => '',
+                        'birthplace' => '',
+                        'sex' => '',
+                        'kiboukamoku' => '',
+                        
+                    ])
+                ]);
+
+                $tougouDbUser->user_social_logins()->saveMany([
+                    new TougouUserSocialLogin([
+                        'line_id' => $lineinfo['providers']['line']['line_id'] ?? '',
+                        'access_tocken' => $lineinfo['providers']['line']['access_token'] ?? '',
+                        'refresh_token' => $lineinfo['providers']['line']['refresh_token'] ?? '',
+                        'remember_token' => $lineinfo['providers']['line']['line_id'] ?? '',
+                        'socialplus_id' => $socailUser['user']['identifier'] ?? '',
+                    ])
+                ]);
+            }
+            dd('Data has been added successfully!');            
 
             return redirect(Config::get('app.app1URL') . 'user/login')->with(['uniqueSocialPlusId' => $uniqueSocialPlusId]);
 
